1f72403a8fad17066c6f82814e416a16720fea0a
Rendiero <workspace.rendy@gmail.com>
2024-09-06 13:04:02 +0700

    full responsive done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}",
-            "./index.html"],
+            "./index.html",
+            "./components/**/*.{html,js,jsx,ts,tsx}",
+            "./image/**/*.{jpg,jpeg,png,svg}",
+            "./src/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
-        'primary': ['Roboto', 'sans-serif'],
+        'primary': ['Roboto', 'sans-serif']
       },
       screens: {
         'sm2x': {'min': '0px', 'max': '639px'}
       }
-    },
+    }
   },
   plugins: [],
 }
